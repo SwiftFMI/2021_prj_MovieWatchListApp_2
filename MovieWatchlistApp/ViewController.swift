d2374fcd5236ff20d869a41074a288a68c7b8178
Kaloyan Dimov <kaloyandimov99@gmail.com>
2022-02-22 14:45:17 +0200

    Initial Commit

---
+++ MovieWatchlistApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieWatchlistApp
+//
+//  Created by Kaloyan Dimov on 22.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
