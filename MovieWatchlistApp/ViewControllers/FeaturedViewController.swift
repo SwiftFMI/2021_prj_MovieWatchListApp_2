340abf88c9eaf4a297cc1df73e1ae81ca9ba03f8
Kaloyan Dimov <kaloyandimov99@gmail.com>
2022-02-24 18:50:20 +0200

    Add Featured View Controller

---
+++ MovieWatchlistApp/ViewControllers/FeaturedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturedViewController.swift
+//  MovieWatchlistApp
+//
+//  Created by Kaloyan Dimov on 24.02.22.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Featured"
+    }
+    
+}
