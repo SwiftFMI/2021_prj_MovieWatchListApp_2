b0d2eb17b11b02d85b1dce2dffd7cfe77ac59803
Kaloyan Dimov <kaloyandimov99@gmail.com>
2022-02-24 18:43:55 +0200

    Add My List View Controller

---
+++ MovieWatchlistApp/ViewControllers/MyListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyListViewController.swift
+//  MovieWatchlistApp
+//
+//  Created by Kaloyan Dimov on 24.02.22.
+//
+
+import UIKit
+
+class MyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "My List"
+    }
+    
+}
