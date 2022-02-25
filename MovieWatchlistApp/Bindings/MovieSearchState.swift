c66c88d13c01a66b129e02872c0c8a91737381da
Dennis <xxyeezusxx@icloud.com>
2022-02-25 13:41:32 +0200

    dataFormatter

--- MovieWatchlistApp/Bindings/MovieSearchState.swift
+++ MovieWatchlistApp/Bindings/MovieSearchState.swift
@@ -11,50 +11,50 @@ import Combine
 import Foundation
 
 class MovieSearchState: ObservableObject {
-    
+
     @Published var query = ""
     @Published var movies: [Movie]?
     @Published var isLoading = false
     @Published var error: NSError?
-    
+
     private var subscriptionToken: AnyCancellable?
-    
+
     let movieService: MovieService
-    
+
     var isEmptyResults: Bool {
         !self.query.isEmpty && self.movies != nil && self.movies!.isEmpty
     }
-    
+
     init(movieService: MovieService = MovieStore.shared) {
         self.movieService = movieService
     }
-    
+
     func startObserve() {
         guard subscriptionToken == nil else { return }
-        
+
         self.subscriptionToken = self.$query
             .map { [weak self] text in
                 self?.movies = nil
                 self?.error = nil
                 return text
-                
+
         }.throttle(for: 1, scheduler: DispatchQueue.main, latest: true)
             .sink { [weak self] in self?.search(query: $0) }
     }
-    
+
     func search(query: String) {
         self.movies = nil
         self.isLoading = false
         self.error = nil
-        
+
         guard !query.isEmpty else {
             return
         }
-        
+
         self.isLoading = true
         self.movieService.searchMovie(query: query) {[weak self] (result) in
             guard let self = self, self.query == query else { return }
-            
+
             self.isLoading = false
             switch result {
             case .success(let response):
@@ -64,7 +64,7 @@ class MovieSearchState: ObservableObject {
             }
         }
     }
-    
+
     deinit {
         self.subscriptionToken?.cancel()
         self.subscriptionToken = nil
